88dd00dd1cffb9d52cc244ef2b97547c71a9dfe9
Sudhakar <sudhakarnraju@gmail.com>
2017-07-28 12:07:40 +0530

    Repo initialized

---
+++ src/main/java/com/cs/lob/waas/collaborationwidget/CollaborationwidgetApplication.java
@@ -0,0 +1,12 @@
+package com.cs.lob.waas.collaborationwidget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborationwidgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborationwidgetApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cs/lob/waas/collaborationwidget/CollaborationwidgetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cs.lob.waas.collaborationwidget;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CollaborationwidgetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
